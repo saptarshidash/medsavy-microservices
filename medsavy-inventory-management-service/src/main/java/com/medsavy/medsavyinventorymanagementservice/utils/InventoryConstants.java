d9b63ac98988687412f0ffc0beed9c5449827025
Saptarshi Das <dsaptarshi@workllama.com>
2022-06-24 18:09:50 +0530

    Added Inventory Constants

---
+++ medsavy-inventory-management-service/src/main/java/com/medsavy/medsavyinventorymanagementservice/utils/InventoryConstants.java
@@ -0,0 +1,12 @@
+package com.medsavy.medsavyinventorymanagementservice.utils;
+
+public class InventoryConstants {
+
+  public static final String INVENTORY_CREATED = "Inventory crated successfully";
+
+  public static final String INVENTORY_CREATION_FAILURE = "Unable to create inventory";
+
+  public static final String MEDICINE_ADD_SUCCESS = "Medicine added successfully";
+
+  public static final String MEDICINE_ADD_FAILURE = "Failed to add medicine";
+}
